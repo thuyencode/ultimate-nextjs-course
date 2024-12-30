42db5795fe7ac6a651204c7e3befd0735d65a3a6
Thuyen Code <thuyencode@proton.me>
2024-12-30 11:33:47 +0700

    Install ESLInt plugin for Tailwind

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
-    extends: ['next', 'love', 'prettier']
+    extends: ['next', 'love', 'prettier', 'plugin:tailwindcss/recommended']
   })
 ]
 
