d7b0c33c714336638de53217d6f6c8c79b3d359f
Thuyen Code <thuyencode@proton.me>
2024-12-31 10:50:17 +0700

    Use array to map out `DropdownMenuItem` elements

--- components/navigation/navbar/theme-toggle.tsx
+++ components/navigation/navbar/theme-toggle.tsx
@@ -7,9 +7,16 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu'
-import { Moon, Sun } from 'lucide-react'
+import { Monitor, Moon, Sun, type LucideIcon } from 'lucide-react'
 import { useTheme } from 'next-themes'
-import type { ReactElement } from 'react'
+import { createElement, type ReactElement } from 'react'
+
+const THEMES: Array<{ theme: 'light' | 'dark' | 'system'; icon: LucideIcon }> =
+  [
+    { theme: 'light', icon: Sun },
+    { theme: 'dark', icon: Moon },
+    { theme: 'system', icon: Monitor }
+  ]
 
 const ThemeToggle = (): ReactElement => {
   const { setTheme } = useTheme()
@@ -20,31 +27,23 @@ const ThemeToggle = (): ReactElement => {
         <Button variant='outline' size='icon'>
           <Sun className='size-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
           <Moon className='absolute size-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+
           <span className='sr-only'>Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align='end'>
-        <DropdownMenuItem
-          onClick={() => {
-            setTheme('light')
-          }}
-        >
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          onClick={() => {
-            setTheme('dark')
-          }}
-        >
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          onClick={() => {
-            setTheme('system')
-          }}
-        >
-          System
-        </DropdownMenuItem>
+
+      <DropdownMenuContent className='capitalize' align='end'>
+        {THEMES.map(({ theme, icon }) => (
+          <DropdownMenuItem
+            key={theme}
+            onClick={() => {
+              setTheme(theme)
+            }}
+          >
+            {createElement(icon, { className: 'transition-all' })}
+            {theme}
+          </DropdownMenuItem>
+        ))}
       </DropdownMenuContent>
     </DropdownMenu>
   )
