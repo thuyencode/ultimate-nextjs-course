120562ce1961f21d2435df6e95f9e516746cf4bd
Thuyen Code <thuyencode@proton.me>
2024-12-31 10:06:41 +0700

    Creating the Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
+import Navbar from '@/components/navigation/navbar'
 import type { ReactElement } from 'react'
 
 export default function Home(): ReactElement {
   return (
     <>
-      <h1 className='h1-bold'>Ultimate Next.js 15 course</h1>
-      <h1 className='h1-bold font-space_grotesk'>Ultimate Next.js 15 course</h1>
+      <Navbar />
     </>
   )
 }
