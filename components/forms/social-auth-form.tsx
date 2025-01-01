8a0b9d655141f52e14a603a8200c94f4af675716
Thuyen Code <thuyencode@proton.me>
2025-01-01 11:04:50 +0700

    Create Auth Layout & Social Auth Form

---
+++ components/forms/social-auth-form.tsx
@@ -0,0 +1,57 @@
+import { cn } from '@/lib/utils'
+import Image, { type ImageProps } from 'next/image'
+import type { ReactElement } from 'react'
+import { Button, type ButtonProps } from '../ui/button'
+
+export const SocialAuthForm = (): ReactElement => (
+  <div className='mt-10 flex flex-wrap gap-2.5'>
+    <SocialAuthButton
+      imageProps={{
+        src: '/icons/github.svg',
+        alt: "Github's logo",
+        className: 'invert-colors'
+      }}
+      text='Log in with GitHub'
+    />
+
+    <SocialAuthButton
+      imageProps={{
+        src: '/icons/google.svg',
+        alt: "Google's logo"
+      }}
+      text='Log in with Google'
+    />
+  </div>
+)
+
+interface SocialAuthButtonProps {
+  imageProps: ImageProps
+  buttonProps?: ButtonProps
+  text: string
+}
+
+function SocialAuthButton({
+  imageProps,
+  buttonProps,
+  text
+}: SocialAuthButtonProps): ReactElement {
+  return (
+    <Button
+      {...buttonProps}
+      className={cn(
+        'background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 rounded-2 px-4 py-3.5',
+        buttonProps?.className
+      )}
+    >
+      <Image
+        width={20}
+        height={20}
+        {...imageProps}
+        alt={imageProps.alt}
+        className={cn('mr-2.5 object-contain', imageProps.className)}
+      />
+
+      <span>{text}</span>
+    </Button>
+  )
+}
