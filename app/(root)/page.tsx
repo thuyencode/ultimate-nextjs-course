910882a724cf0494ee14d81c011226d0ce329537
Thuyen Code <thuyencode@proton.me>
2025-01-04 09:28:24 +0700

    Home Layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,26 +1,28 @@
-import { auth, signOut } from '@/auth'
 import { Button } from '@/components/ui/button'
 import { ROUTES } from '@/constants'
+import Link from 'next/link'
 import type { ReactElement } from 'react'
 
-const HomePage = async (): Promise<ReactElement> => {
-  const session = await auth()
+const HomePage = (): ReactElement => (
+  <>
+    <section className='flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center'>
+      <h1 className='h1-bold text-dark100_light900'>All Questions</h1>
 
-  console.log(session)
-
-  return (
-    <>
-      <form
-        className='px-10 pt-[100px]'
-        action={async () => {
-          'use server'
-          await signOut({ redirectTo: ROUTES.LOG_IN })
-        }}
+      <Button
+        className='primary-gradient min-h-[46px] px-4 py-3 !text-light-900'
+        asChild
       >
-        <Button type='submit'>Sign out</Button>
-      </form>
-    </>
-  )
-}
+        <Link href={ROUTES.ASK}>Ask a question</Link>
+      </Button>
+    </section>
+    <section className='mt-11'>Local Search</section>
+    Home Filters
+    <div className='mt-10 flex w-full flex-col gap-6'>
+      <p>QUestion Card 1</p>
+      <p>QUestion Card 2</p>
+      <p>QUestion Card 3</p>
+    </div>
+  </>
+)
 
 export default HomePage
